f20051bc0bee51763ab06dbc9836670944df2047
sebastnchan <vauntex18@gmail.com>
2020-12-28 20:28:39 -0300

    Load configurations method.

---
+++ src/main/java/club/rigox/staffcore/utils/Config.java
@@ -0,0 +1,46 @@
+package club.rigox.staffcore.utils;
+
+import club.rigox.staffcore.StaffCore;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import static club.rigox.staffcore.utils.Logger.error;
+import static club.rigox.staffcore.utils.Logger.warn;
+
+public class Config {
+    private final StaffCore plugin;
+
+    public Config(StaffCore plugin) {
+        this.plugin = plugin;
+    }
+
+    public FileConfiguration createConfig(String configName) {
+        File configFile = new File(plugin.getDataFolder(), configName + ".yml");
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            plugin.saveResource(configName + ".yml", false);
+        }
+
+        FileConfiguration cfg = new YamlConfiguration();
+        try {
+            cfg.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            warn(String.format("A error occurred while copying the config %s.yml to the plugin data folder. Error: %s", configName, e));
+            e.printStackTrace();
+        }
+        return cfg;
+    }
+
+    public static String getLangString(String path) {
+        if (StaffCore.instance.getLang().getString(path) == null) {
+            error(String.format("The folowing path doesn't exists on Lang.yml! %s, please add it.", path));
+            return "&cPlease inform this error to an administrator and let it know to review the console.";
+        }
+        return StaffCore.instance.getLang().getString(Objects.requireNonNull(path));
+    }
+}
