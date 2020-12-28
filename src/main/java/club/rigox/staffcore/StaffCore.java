5fbeca58e6dd5d122e537d44f7b4e37498a32571
sebastnchan <vauntex18@gmail.com>
2020-12-28 19:33:32 -0300

    Well

---
+++ src/main/java/club/rigox/staffcore/StaffCore.java
@@ -0,0 +1,17 @@
+package club.rigox.staffcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
