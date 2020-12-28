48f49865910fa9cda03463cfdf2064d7112f1d65
sebastnchan <vauntex18@gmail.com>
2020-12-28 20:29:22 -0300

    config load method and lang getter

--- src/main/java/club/rigox/staffcore/StaffCore.java
+++ src/main/java/club/rigox/staffcore/StaffCore.java
@@ -1,13 +1,28 @@
 package club.rigox.staffcore;
 
+import club.rigox.staffcore.utils.Config;
 import co.aikar.commands.PaperCommandManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StaffCore extends JavaPlugin {
     public static StaffCore instance;
 
+    private FileConfiguration lang;
+
     @Override
     public void onEnable() {
         instance = this;
+
+        loadConfiguration();
+    }
+
+    public void loadConfiguration() {
+        Config config = new Config(this);
+        lang = config.createConfig("lang");
+    }
+
+    public FileConfiguration getLang() {
+        return lang;
     }
 }
