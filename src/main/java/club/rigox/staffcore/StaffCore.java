0a365a1e0bb882bae0e0c144d7deddecbc0d7aa5
sebastnchan <vauntex18@gmail.com>
2020-12-29 11:53:58 -0300

    Alright

--- src/main/java/club/rigox/staffcore/StaffCore.java
+++ src/main/java/club/rigox/staffcore/StaffCore.java
@@ -1,6 +1,8 @@
 package club.rigox.staffcore;
 
+import club.rigox.staffcore.commands.InvCMD;
 import club.rigox.staffcore.database.MongoDB;
+import club.rigox.staffcore.player.InventoryUtil;
 import club.rigox.staffcore.utils.Config;
 import co.aikar.commands.PaperCommandManager;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,6 +12,7 @@ public final class StaffCore extends JavaPlugin {
     public static StaffCore instance;
 
     private MongoDB mongo;
+    private InventoryUtil inventoryUtil;
 
     private FileConfiguration lang;
     private FileConfiguration database;
@@ -19,9 +22,11 @@ public final class StaffCore extends JavaPlugin {
         instance = this;
 
         mongo = new MongoDB();
+        inventoryUtil = new InventoryUtil(this);
 
         loadConfiguration();
         mongo.connect();
+        registerCommands();
     }
 
     @Override
@@ -35,6 +40,10 @@ public final class StaffCore extends JavaPlugin {
         database = config.createConfig("database");
     }
 
+    public void registerCommands() {
+        PaperCommandManager manager = new PaperCommandManager(this);
+        manager.registerCommand(new InvCMD(this));
+    }
 
     public FileConfiguration getLang() {
         return lang;
@@ -42,5 +51,13 @@ public final class StaffCore extends JavaPlugin {
 
     public FileConfiguration getDatabase() {
         return database;
+    }
+
+    public MongoDB getMongo() {
+        return mongo;
+    }
+
+    public InventoryUtil getInventoryUtil() {
+        return inventoryUtil;
     }
 }
